9786d5e6d2ad66fad6c2cc770b669a63a107154a
soeiroo <friskylt8@gmail.com>
2025-06-19 17:56:08 -0300

    add-triagem

--- js/triagem.js
+++ js/triagem.js
@@ -1,4 +1,40 @@
 import { carregarPacientes } from './pacienteStorage.js';
 
-carregarPacientes();
+function listarPacientes() {
+    const lista = document.getElementById('lista-pacientes');
+    lista.innerHTML = '<p>Lista de pacientes</p>';
+
+    const pacientes = carregarPacientes();
+
+    if (pacientes.length === 0) {
+        lista.innerHTML = '<p>Nenhum paciente foi cadastrado.</p>';
+        return;
+    }
+
+    pacientes.forEach((paciente) => {
+        const div = document.createElement('div');
+        div.className = 'card';
+
+        const nomeElemento = document.createElement('strong');
+        nomeElemento.innerText = paciente.nome;
+        nomeElemento.style.cursor = 'pointer';
+
+        const motivoElemento = document.createElement('p');
+        motivoElemento.innerText = paciente.motivo;
+        motivoElemento.style.display = 'none';
+        motivoElemento.style.margin = '8px 0 0 0';
+
+        nomeElemento.addEventListener('click', () => {
+            motivoElemento.style.display = motivoElemento.style.display === 'none' ? 'block' : 'none';
+        })
+
+        div.appendChild(nomeElemento);
+        div.appendChild(motivoElemento);
+        lista.appendChild(div);
+
+    })
+
+}
+
+listarPacientes();
 
