65dceef34ef6a1725df1e45e0f6731e532d153e6
samylesousa <samylesousa@alu.ufc.br>
2025-06-19 15:03:39 -0300

    pagina de cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,61 @@
+const formulario = document.getElementById('formulario-paciente');
+const lista = document.getElementById('lista-pacientes');
+
+
+function salvarPacientes(pacientes) {
+  localStorage.setItem('pacientes', JSON.stringify(pacientes));
+}
+
+function carregarPacientes() {
+  const dados = localStorage.getItem('pacientes');
+  if(dados){
+    return JSON.parse(dados);
+  }else{
+    return [];
+  }
+}
+
+function listarPacientes() {
+  const pacientes = carregarPacientes();
+  lista.innerHTML = '';
+
+  if (pacientes.length === 0) {
+    lista.innerHTML = '<p>Nenhum paciente foi cadastrado.</p>';
+    return;
+  }
+
+  pacientes.map((p) => {
+    const div = document.createElement('div');
+    div.className = 'card';
+    div.innerHTML = `<strong>${p.nome}</strong><br><p>${p.motivo}</p>`;
+    lista.appendChild(div);
+  });
+}
+
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById('nome').value.trim();
+  const motivo = document.getElementById('motivo').value.trim();
+
+  if(!nome || !motivo){
+    return;
+  } 
+
+  const novoPaciente = {
+    id: Date.now(),
+    nome,
+    motivo
+  };
+
+  const pacientes = carregarPacientes();
+  pacientes.push(novoPaciente);
+  salvarPacientes(pacientes);
+
+  formulario.reset();
+  listarPacientes();
+});
+
+
+listarPacientes();
